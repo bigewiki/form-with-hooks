d89cbcd7b003233033dfe75567c19c9937fef1cf
root <root@LAPTOP-441568VE.localdomain>
2019-03-03 22:51:16 -0700

    added state, passed it down, now accessing it from a hook

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import RequestPictures from './components/RequestPictures';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { messageFromState: "Hello from state" };
+  }
+
   render() {
     return (
       <div className="App">
-      <RequestPictures/>
+      <RequestPictures props={this.state.messageFromState}/>
       </div>
     );
   }
--- src/components/RequestPictures.js
+++ src/components/RequestPictures.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
-function RequestPictures() {
+function RequestPictures(messageFromState) {
+
+  const [getMessageFromState, setMessageFromState] = useState(messageFromState);
 
   return (
     <div>
@@ -12,7 +14,7 @@ function RequestPictures() {
         <label for="frmEmailC">Confirm Email</label><br/>
         <input className="_3919_input" type="email" name="emailC" id="frmEmailC" placeholder="name@example.com" required autocomplete="email"/><br/>
         <label for="whichPics">Which pictures did you want?</label><br/>
-        <textarea name="whichPics"></textarea>
+        <textarea name="whichPics">{getMessageFromState.props}</textarea>
         <button>Submit</button>
       </form>
     </div>
