a5718c46d5c0c998cb8bb512470f49963fbd3578
Edward M <edwardtmuniz@gmail.com>
2019-03-06 23:09:51 -0700

    now sending post request to php file to handle google response

--- src/components/RequestPictures.js
+++ src/components/RequestPictures.js
@@ -1,19 +1,50 @@
 import React, { useState } from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const RequestPictures = () => {
+
+  //setting initial state
+  let [captchaState, setCaptchaState] = useState();
+
+  // // // // //
+  // RECAPTCHA
+  // // // // //
+
+  const SITE_KEY = "6LfXwJUUAAAAAA6goGerxF04zaKfpK1JzDnvlib8";
+
+  const onChange = (value) => {
+    setCaptchaState(value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://3919.muniz.dev/wp-content/themes/astra-child/recaptcha.php',{
+      method: 'post',
+      body: captchaState,
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
 
   return (
     <div>
       <form>
-        <label for="frmNameA">Name</label><br/>
-        <input className="_3919_input" type="name" name="name" id="frmNameA" placeholder="Full name" required autocomplete="name"/><br/>
-        <label for="frmEmailA">Email</label><br/>
-        <input className="_3919_input" type="email" name="email" id="frmEmailA" placeholder="name@example.com" required autocomplete="email"/><br/>
-        <label for="frmEmailC">Confirm Email</label><br/>
-        <input className="_3919_input" type="email" name="emailC" id="frmEmailC" placeholder="name@example.com" required autocomplete="email"/><br/>
-        <label for="whichPics">Which pictures did you want?</label><br/>
-        <textarea name="whichPics"></textarea>
-        <button>Submit</button>
+        <label htmlFor="frmNameA">Name</label><br/>
+        <input className="_3919_input" type="name" name="name" id="frmNameA" placeholder="Full name" required autoComplete="name"/><br/>
+        <label htmlFor="frmEmailA">Email</label><br/>
+        <input className="_3919_input" type="email" name="email" id="frmEmailA" placeholder="name@example.com" required autoComplete="email"/><br/>
+        <label htmlFor="frmEmailC">Confirm Email</label><br/>
+        <input className="_3919_input" type="email" name="emailC" id="frmEmailC" placeholder="name@example.com" required autoComplete="email"/><br/>
+        <label htmlFor="whichPics">Which pictures did you want?</label><br/>
+        <textarea name="whichPics"></textarea><br/><br/>
+        <ReCAPTCHA
+          sitekey={SITE_KEY}
+          onChange={onChange}
+        /><br/>
+        <button onClick={onSubmit}>Submit</button>
       </form>
     </div>
   );
